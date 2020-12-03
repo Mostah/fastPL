c3e83fc96e8df2b14a319b2b680fd20c54483940
thibmonsel <thibaultmonsel@gmail.com>
2020-12-03 16:50:55 +0100

    'took out main from test_example"

--- test/test_example.cpp
+++ test/test_example.cpp
@@ -4,16 +4,9 @@
 #include <sstream>
  
 TEST( TestNombre, TestNombreVide )
-{
+{ 
     Nombre n;
     std::ostringstream os;
     os << n;
     EXPECT_EQ( os.str(), "0" );
 }
-
-int main( int argc, char * argv[] )
-{
-    ::testing::InitGoogleTest( &argc, argv );
-    return RUN_ALL_TESTS();
-}
- 
