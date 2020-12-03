4c76c9ad8c1588ee62b5e023a5109f334f50d966
Dixneuf Emma <emma_dixneuf@DESKTOP-V4QD6C9.localdomain>
2020-12-03 11:50:04 +0100

    created some test files

---
+++ test/test_example.cpp
@@ -0,0 +1,28 @@
+#include "gtest/gtest.h"
+#include "../src/example.h"
+
+namespace {
+
+class MyIntTest : public ::testing::Test{};
+
+TEST_F(MyIntTest, isOdd)
+{
+  MyInt mi1 = MyInt(10);
+  EXPECT_EQ(false, mi1.isOdd());
+
+  MyInt mi2 = MyInt(13);
+  EXPECT_EQ(true, mi2.isOdd());
+}
+
+
+TEST_F(MyIntTest, isEven)
+{
+  MyInt mi1 = MyInt(10);
+  EXPECT_EQ(true, mi1.isEven());
+
+  MyInt mi2 = MyInt(13);
+  EXPECT_EQ(false, mi2.isEven());
+}
+
+} // namespace
+
