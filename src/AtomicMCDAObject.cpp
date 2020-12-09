d93986da79550e533daa2ee1dd19039c99f7d93c
Mostah <thomas.vindard@orange.fr>
2020-12-09 12:07:21 +0100

    Add CriterionValue type, tests and AtomicMCDAObject type

---
+++ src/AtomicMCDAObject.cpp
@@ -0,0 +1,7 @@
+#include "AtomicMCDAObject.h"
+
+AtomicMCDAObject::AtomicMCDAObject() { ++nb_instances_; };
+
+AtomicMCDAObject::~AtomicMCDAObject() { --nb_instances_; };
+
+int AtomicMCDAObject::get_nb_instances() { return nb_instances_; };
