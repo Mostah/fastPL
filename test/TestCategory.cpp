eeb45269c4673c04d1483e9a70ebbae025c2b315
Thibault Monsel <thibaultmonsel@gmail.com>
2020-12-09 12:31:38 +0100

    Added Category type and associated tests on branch

---
+++ test/TestCategory.cpp
@@ -0,0 +1,30 @@
+#include "../src/Category.h"
+#include "gtest/gtest.h"
+#include <sstream>
+#include <utility>
+
+
+TEST(TestCategory, TestCategory) {
+  Category category1("cat0", 0);
+  std::ostringstream os;
+  os << category1;
+  EXPECT_EQ(os.str(), "Category(cat0:0)");
+}
+
+TEST(TestCategory, TestCategory2) {
+  Category category1("cat12", 12);
+  std::ostringstream os;
+  os << category1;
+  EXPECT_EQ(os.str(), "Category(cat12:12)");
+}
+
+
+TEST(TestCategory, TestConstructorByCopy) {
+  Category category1("cat1", 13);
+
+  Category catCopied = Category(category1);
+
+  std::ostringstream os;
+  os << catCopied;
+  EXPECT_EQ(os.str(), "Category(cat1:13)");
+}
