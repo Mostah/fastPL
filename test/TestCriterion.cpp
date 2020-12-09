11a07b343b0b122b33f0c88fe77039c91bd159de
Dixneuf Emma <emma_dixneuf@DESKTOP-V4QD6C9.localdomain>
2020-12-09 16:11:20 +0100

    criterion constructor tests

--- test/TestCriterion.cpp
+++ test/TestCriterion.cpp
@@ -1,13 +1,45 @@
-#include "../src/example.h"
 #include "../src/Criterion.h"
 #include "gtest/gtest.h"
 #include <sstream>
 #include <utility>
 
+int AtomicMCDAObject::nb_instances_{0};
 
-TEST(TestCriterion, TestCriterionVide) {
-  Criterion crit = Criterion("2");
+TEST(TestCriterion, TestBaseConstructorWithoutSpec) {
+  std::string id = "test_no_spec";
+  Criterion crit = Criterion(id);
   std::ostringstream os;
   os << crit;
-  EXPECT_EQ(os.str(), "Criterion(2:+)");
+  EXPECT_EQ(os.str(), "Criterion(id : test_no_spec, name : , direction : +, weight : 0)");
 }
+
+TEST(TestCriterion, TestBaseConstructor) {
+  std::string id = "test";
+  std::string name = "criterion_test";
+  int direction = -1;
+  float weight = 0.4;
+  Criterion crit = Criterion(id, name, direction, weight);
+  std::ostringstream os;
+  os << crit;
+  EXPECT_EQ(os.str(), "Criterion(id : test, name : criterion_test, direction : -, weight : 0.4)");
+}
+
+TEST(TestCriterion, TestConstructorByCopy) {
+  std::string id = "test_copy";
+  std::string name = "criterion_by_copy";
+  int direction = 1;
+  float weight = 0.3;
+  Criterion crit = Criterion(id, name, direction, weight);
+
+  Criterion critCopied = Criterion(crit);
+
+  std::ostringstream os;
+  os << critCopied;
+  EXPECT_EQ(os.str(), "Criterion(id : test_copy, name : criterion_by_copy, direction : +, weight : 0.3)");
+}
+
+TEST(TestCriterion, TestAllInstancesDestroyed) {
+  EXPECT_EQ(AtomicMCDAObject::get_nb_instances(), 0);
+}
+
+
--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -1,4 +1,4 @@
-#include "TestCriterionValue.cpp"
+#include "TestCriterion.cpp"
 #include "gtest/gtest.h"
 #include <sstream>
 #include <utility>
