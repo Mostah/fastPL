ec0b0ee5bbd537dc2ba838d1a35e02b0e828a419
Mostah <thomas.vindard@orange.fr>
2020-12-10 11:25:47 +0100

    Add criteria structure with base methods

--- src/Criteria.h
+++ src/Criteria.h
@@ -1,43 +1,75 @@
 #ifndef CRITERIA_H
 #define CRITERIA_H
 
+#include "Criterion.h"
 #include <iostream>
 #include <vector>
-#include <Criterion>
 
 class Criteria {
-    public:
-    /**
-     * Criteria constructor with defined vector of criteria
-     *
-     * @param criteria_list Vector of criteria
-     */
-    Criteria(std::vector<Criterion> criteria_list);
+public:
+  /**
+   * Criteria constructor with defined vector of criteria
+   *
+   * @param criterion_vect Vector of criteria
+   */
+  Criteria(std::vector<Criterion> &criterion_vect);
 
-    /**
-     * Criteria constructor without vector
-     *
-     * @param nb_of_criteria Number of criteria
-     * @param prefix Prefix to identify criteria
-     */
-    Criteria(int nb_of_criteria, std::string prefix);
+  /**
+   * Criteria constructor without vector
+   *
+   * @param nb_of_criterion Number of criteria
+   * @param prefix Prefix to identify criteria
+   */
+  Criteria(int nb_of_criterion, std::string prefix);
 
-    /**
-     * Criteria constructor by copy
-     *
-     * @param crits Based criteria to copy
-     */
-    Criteria(const Criteria &crits);
+  /**
+   * Criteria constructor by copy
+   *
+   * @param crits Based criteria to copy
+   */
+  Criteria(const Criteria &crits);
 
-    ~Criteria();
-    friend std::ostream & operator <<( std::ostream & out, const Criteria & crits );
+  ~Criteria();
+
+  friend std::ostream &operator<<(std::ostream &out, const Criteria &crits);
+
+  /**
+   * setCriterionVect setter of criterion vector parameter
+   *
+   * @param criterion_vect_
+   */
+  void setCriterionVect(std::vector<Criterion> &criterion_vect_);
+
+  /**
+   * getCriterionVect getter of criterion vector parameter
+   *
+   * @return criterion_vect_
+   */
+  std::vector<Criterion> getCriterionVect() const;
+
+  /**
+   * getMinWeight return the min criterion weight of this Criteria structure
+   *
+   * @return min_weight
+   */
+  float getMinWeight();
+
+  /**
+   * getMaxWeight return the max criterion weight of this Criteria structure
+   *
+   * @return max_weight
+   */
+  float getMaxWeight();
+
+  /**
+   * getSumWeight return the sum of criterion weight of this Criteria structure
+   *
+   * @return sum_weight
+   */
+  float getSumWeight();
 
 private:
-
-    std::vector<Criterion> criteria_list_;
-    int nb_of_criteria_;
-    std::string prefix_;
+  std::vector<Criterion> criterion_vect_;
 };
-
 
 #endif
---
+++ test/TestCriteria.cpp
@@ -0,0 +1,120 @@
+#include "../src/Criteria.h"
+#include "../src/Criterion.h"
+#include "gtest/gtest.h"
+#include <sstream>
+#include <utility>
+
+TEST(TestCriteria, TestBaseConstructorWithNbAndId) {
+  std::string id = "test";
+  Criteria crits = Criteria(2, id);
+  std::ostringstream os;
+  os << crits;
+  EXPECT_EQ(os.str(), "Criteria(Criterion(id : test0, name : , "
+                      "direction : +, weight : 0), Criterion(id : "
+                      "test1, name : , direction : +, weight : 0), )");
+}
+
+TEST(TestCriteria, TestCriterionVectConstructor) {
+  std::string id = "test";
+  std::string name = "criterion_test";
+  int direction = -1;
+  float weight = 0.4;
+  Criterion crit = Criterion(id, name, direction, weight);
+
+  std::vector<Criterion> crit_vect;
+  crit_vect.push_back(crit);
+
+  Criteria crits = Criteria(crit_vect);
+
+  std::ostringstream os;
+  os << crits;
+  EXPECT_EQ(os.str(), "Criteria(Criterion(id : test, name : criterion_test, "
+                      "direction : -, weight : 0.4), )");
+}
+
+TEST(TestCriteria, TestCopyConstructor) {
+  std::string id = "test";
+  std::string name = "criterion_test";
+  int direction = -1;
+  float weight = 0.4;
+  Criterion crit = Criterion(id, name, direction, weight);
+
+  std::vector<Criterion> crit_vect;
+  crit_vect.push_back(crit);
+
+  Criteria crits = Criteria(crit_vect);
+
+  Criteria crits_copied = Criteria(crits);
+
+  std::ostringstream os;
+  os << crits_copied;
+  EXPECT_EQ(os.str(), "Criteria(Criterion(id : test, name : criterion_test, "
+                      "direction : -, weight : 0.4), )");
+}
+
+TEST(TestCriteria, TestSetCriterionVect) {
+  std::string id = "test";
+  std::string name = "criterion_test";
+  int direction = -1;
+  float weight = 0.4;
+  Criterion crit = Criterion(id, name, direction, weight);
+
+  std::vector<Criterion> crit_vect;
+  crit_vect.push_back(crit);
+
+  Criteria crits = Criteria(2, id);
+  crits.setCriterionVect(crit_vect);
+
+  std::ostringstream os;
+  os << crits;
+  EXPECT_EQ(os.str(), "Criteria(Criterion(id : test, name : criterion_test, "
+                      "direction : -, weight : 0.4), )");
+}
+
+TEST(TestCriteria, TestGetMaxWeight) {
+  std::vector<Criterion> crit_vect0;
+  Criteria crits = Criteria(crit_vect0);
+
+  float max_weight = crits.getMaxWeight();
+  EXPECT_FLOAT_EQ(0, max_weight);
+
+  std::vector<Criterion> crit_vect1;
+  crit_vect1.push_back(Criterion("test0", "", -1, 0.4));
+  crit_vect1.push_back(Criterion("test1", "", -1, 0.6));
+  crits.setCriterionVect(crit_vect1);
+
+  max_weight = crits.getMaxWeight();
+  EXPECT_FLOAT_EQ(0.6, max_weight);
+}
+
+TEST(TestCriteria, TestGetMinWeight) {
+  std::vector<Criterion> crit_vect0;
+  Criteria crits = Criteria(crit_vect0);
+
+  float min_weight = crits.getMinWeight();
+  EXPECT_FLOAT_EQ(0, min_weight);
+
+  std::vector<Criterion> crit_vect1;
+  crit_vect1.push_back(Criterion("test0", "", -1, 0.4));
+  crit_vect1.push_back(Criterion("test1", "", -1, 0.6));
+  crits.setCriterionVect(crit_vect1);
+
+  min_weight = crits.getMinWeight();
+  EXPECT_FLOAT_EQ(0.4, min_weight);
+}
+
+TEST(TestCriteria, TestGetSumWeight) {
+  std::vector<Criterion> crit_vect0;
+  Criteria crits = Criteria(crit_vect0);
+
+  float sum_weight = crits.getSumWeight();
+  EXPECT_FLOAT_EQ(0, sum_weight);
+
+  std::vector<Criterion> crit_vect1;
+  crit_vect1.push_back(Criterion("test0", "", -1, 0.4));
+  crit_vect1.push_back(Criterion("test1", "", -1, 0.6));
+  crits.setCriterionVect(crit_vect1);
+
+  sum_weight = crits.getSumWeight();
+  EXPECT_FLOAT_EQ(1, sum_weight);
+}
