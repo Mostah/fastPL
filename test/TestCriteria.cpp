8f45e79482da229936cad43a398375cdf0e608a9
Dixneuf Emma <emma_dixneuf@DESKTOP-V4QD6C9.localdomain>
2020-12-17 12:47:01 +0100

    Added operator [] and default prefix to constructor

--- test/TestCriteria.cpp
+++ test/TestCriteria.cpp
@@ -32,6 +32,15 @@ TEST(TestCriteria, TestCriterionVectConstructor) {
                       "direction : -, weight : 0.4), )");
 }
 
+TEST(TestCriteria, TestBaseConstructorWithNbOnly) {
+  Criteria criteria1 = Criteria(2);
+  std::ostringstream os;
+  os << criteria1;
+  EXPECT_EQ(os.str(), "Criteria(Criterion(id : crit0, name : , "
+                      "direction : +, weight : 0), Criterion(id : crit1, name : , "
+                      "direction : +, weight : 0), )");
+}
+
 TEST(TestCriteria, TestCopyConstructor) {
   std::string id = "test";
   std::string name = "criterion_test";
@@ -118,6 +127,27 @@ TEST(TestCriteria, TestGetSumWeight) {
   sum_weight = crits.getSumWeight();
   EXPECT_FLOAT_EQ(1, sum_weight);
 }
+
+TEST(TestCriteria, TestOperatorSubscript) {
+  Criteria criteria1 = Criteria(1);
+  Criterion crit = criteria1[0];
+
+  std::ostringstream os;
+  os << crit;
+  EXPECT_EQ(os.str(), "Criterion(id : crit0, name : , "
+                      "direction : +, weight : 0)");
+}
+
+TEST(TestCriteria, TestOperatorConstantSubscript) {
+  const Criteria criteria1 = Criteria(1);
+  Criterion crit = criteria1[0];
+
+  std::ostringstream os;
+  os << crit;
+  EXPECT_EQ(os.str(), "Criterion(id : crit0, name : , "
+                      "direction : +, weight : 0)");
+}
+
 
 TEST(TestCriteria, TestAllInstancesDestroyed) {
   EXPECT_EQ(AtomicMCDAObject::get_nb_instances(), 0);
