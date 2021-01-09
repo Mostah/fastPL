475ee64892203cc9c1f15037860ef8535442e70a
Emma Dixneuf <emma.dixneuf@student.ecp.fr>
2021-01-09 10:35:48 +0100

    corrected nomenclature of alt and crit

--- test/TestAlternativesPerformance.cpp
+++ test/TestAlternativesPerformance.cpp
@@ -7,44 +7,44 @@
 #include <utility>
 
 TEST(TestAlternativesPerformance, TestConstructorBaseConstructor) {
-  Criteria crit = Criteria(2, "a");
+  Criteria crit = Criteria(2, "crit");
 
-  AlternativesPerformance alt_perf = AlternativesPerformance(2, crit, "test");
+  AlternativesPerformance alt_perf = AlternativesPerformance(2, crit, "a");
   std::ostringstream os;
   os << alt_perf;
   EXPECT_EQ(
       os.str(),
       "AlternativesPerformance( PerformanceTable[ Performance: Perf( name : "
-      "test0, crit : a0, value : 0 ) Perf( name : test0, crit : a1, value : 0 "
-      ") | Performance: Perf( name : test1, crit : a0, value : 0 ) Perf( name "
-      ": test1, crit : a1, value : 0 ) | ], AlternativesAssignment{ test0->"
-      " test1-> }");
+      "a0, crit : crit0, value : 0 ) Perf( name : a0, crit : crit1, value : 0 "
+      ") | Performance: Perf( name : a1, crit : crit0, value : 0 ) Perf( name "
+      ": a1, crit : crit1, value : 0 ) | ], AlternativesAssignment{ a0->"
+      " a1-> }");
   EXPECT_EQ(alt_perf.getMode(), "alt");
 
   std::map<std::string, std::string> map =
-      std::map<std::string, std::string>{{"test0", "cat0"}, {"test1", "cat1"}};
+      std::map<std::string, std::string>{{"a0", "cat0"}, {"a1", "cat1"}};
   AlternativesPerformance alt_perf2 =
-      AlternativesPerformance(2, crit, "test", map);
+      AlternativesPerformance(2, crit, "a", map);
   std::ostringstream os2;
   os2 << alt_perf2;
   EXPECT_EQ(
       os2.str(),
       "AlternativesPerformance( PerformanceTable[ Performance: Perf( name : "
-      "test0, crit : a0, value : 0 ) Perf( name : test0, crit : a1, value : 0 "
-      ") | Performance: Perf( name : test1, crit : a0, value : 0 ) Perf( name "
-      ": test1, crit : a1, value : 0 ) | ], AlternativesAssignment{ test0->cat0"
-      " test1->cat1 }");
+      "a0, crit : crit0, value : 0 ) Perf( name : a0, crit : crit1, value : 0 "
+      ") | Performance: Perf( name : a1, crit : crit0, value : 0 ) Perf( name "
+      ": a1, crit : crit1, value : 0 ) | ], AlternativesAssignment{ a0->cat0"
+      " a1->cat1 }");
   EXPECT_EQ(alt_perf2.getMode(), "alt");
 }
 
 TEST(TestAlternativesPerformance, TestConstructorMapErrors) {
-  Criteria crit = Criteria(2, "a");
+  Criteria crit = Criteria(2, "crit");
 
   std::map<std::string, std::string> map_err =
-      std::map<std::string, std::string>{{"test10", "cat0"}, {"test5", "cat1"}};
+      std::map<std::string, std::string>{{"a10", "cat0"}, {"a5", "cat1"}};
   try {
     AlternativesPerformance alt_perf_err =
-        AlternativesPerformance(2, crit, "test", map_err);
+        AlternativesPerformance(2, crit, "a", map_err);
     FAIL() << "should have thrown invalid argument.";
   } catch (std::invalid_argument const &err) {
     EXPECT_EQ(err.what(), std::string("The alternatives in the map should be "
@@ -56,46 +56,46 @@ TEST(TestAlternativesPerformance, TestConstructorMapErrors) {
 
 TEST(TestAlternativesPerformance, TestConstructorWithPerfVect) {
   std::vector<Performance> perf_vect;
-  Criteria crit = Criteria(2, "a");
-  perf_vect.push_back(Performance(crit, "test0"));
-  perf_vect.push_back(Performance(crit, "test1"));
+  Criteria crit = Criteria(2, "crit");
+  perf_vect.push_back(Performance(crit, "a0"));
+  perf_vect.push_back(Performance(crit, "a1"));
 
   AlternativesPerformance alt_perf = AlternativesPerformance(perf_vect);
   std::ostringstream os;
   os << alt_perf;
   EXPECT_EQ(os.str(),
             "AlternativesPerformance( PerformanceTable[ Performance: Perf( "
-            "name : test0, crit : a0, value "
-            ": 0 ) Perf( name : test0, crit : a1, value : 0 ) | "
-            "Performance: Perf( name : test1, crit : a0, value : 0 ) Perf( "
-            "name : test1, crit : a1, value : 0 ) | ], AlternativesAssignment{ "
-            "test0->"
-            " test1-> }");
+            "name : a0, crit : crit0, value "
+            ": 0 ) Perf( name : a0, crit : crit1, value : 0 ) | "
+            "Performance: Perf( name : a1, crit : crit0, value : 0 ) Perf( "
+            "name : a1, crit : crit1, value : 0 ) | ], AlternativesAssignment{ "
+            "a0->"
+            " a1-> }");
   EXPECT_EQ(alt_perf.getMode(), "alt");
 
   std::map<std::string, std::string> map =
-      std::map<std::string, std::string>{{"test0", "cat0"}, {"test1", "cat1"}};
+      std::map<std::string, std::string>{{"a0", "cat0"}, {"a1", "cat1"}};
 
   AlternativesPerformance alt_perf2 = AlternativesPerformance(perf_vect, map);
   std::ostringstream os2;
   os2 << alt_perf2;
   EXPECT_EQ(os2.str(),
             "AlternativesPerformance( PerformanceTable[ Performance: Perf( "
-            "name : test0, crit : a0, value "
-            ": 0 ) Perf( name : test0, crit : a1, value : 0 ) | "
-            "Performance: Perf( name : test1, crit : a0, value : 0 ) Perf( "
-            "name : test1, crit : a1, value : 0 ) | ], AlternativesAssignment{ "
-            "test0->cat0"
-            " test1->cat1 }");
+            "name : a0, crit : crit0, value "
+            ": 0 ) Perf( name : a0, crit : crit1, value : 0 ) | "
+            "Performance: Perf( name : a1, crit : crit0, value : 0 ) Perf( "
+            "name : a1, crit : crit1, value : 0 ) | ], AlternativesAssignment{ "
+            "a0->cat0"
+            " a1->cat1 }");
 }
 
 TEST(TestAlternativesPerformance, TestConstructorWithPerfVectMapErrors) {
   std::vector<Performance> perf_vect;
-  Criteria crit = Criteria(2, "a");
-  perf_vect.push_back(Performance(crit, "test0"));
-  perf_vect.push_back(Performance(crit, "test1"));
+  Criteria crit = Criteria(2, "crit");
+  perf_vect.push_back(Performance(crit, "a0"));
+  perf_vect.push_back(Performance(crit, "a1"));
   std::map<std::string, std::string> map =
-      std::map<std::string, std::string>{{"test12", "cat0"}, {"test1", "cat1"}};
+      std::map<std::string, std::string>{{"a12", "cat0"}, {"a1", "cat1"}};
   try {
     AlternativesPerformance alt_perf = AlternativesPerformance(perf_vect, map);
     FAIL() << "should have thrown invalid argument.";
@@ -109,9 +109,9 @@ TEST(TestAlternativesPerformance, TestConstructorWithPerfVectMapErrors) {
 
 TEST(TestAlternativesPerformance, TestConstructorWithPerfTable) {
   std::vector<Performance> perf_vect;
-  Criteria crit = Criteria(2, "a");
-  perf_vect.push_back(Performance(crit, "test0"));
-  perf_vect.push_back(Performance(crit, "test1"));
+  Criteria crit = Criteria(2, "crit");
+  perf_vect.push_back(Performance(crit, "a0"));
+  perf_vect.push_back(Performance(crit, "a1"));
   PerformanceTable perf_table = PerformanceTable(perf_vect);
 
   AlternativesPerformance alt_perf = AlternativesPerformance(perf_table);
@@ -120,37 +120,37 @@ TEST(TestAlternativesPerformance, TestConstructorWithPerfTable) {
   os << alt_perf;
   EXPECT_EQ(os.str(),
             "AlternativesPerformance( PerformanceTable[ Performance: Perf( "
-            "name : test0, crit : a0, value "
-            ": 0 ) Perf( name : test0, crit : a1, value : 0 ) | "
-            "Performance: Perf( name : test1, crit : a0, value : 0 ) Perf( "
-            "name : test1, crit : a1, value : 0 ) | ], AlternativesAssignment{ "
-            "test0-> test1-> }");
+            "name : a0, crit : crit0, value "
+            ": 0 ) Perf( name : a0, crit : crit1, value : 0 ) | "
+            "Performance: Perf( name : a1, crit : crit0, value : 0 ) Perf( "
+            "name : a1, crit : crit1, value : 0 ) | ], AlternativesAssignment{ "
+            "a0-> a1-> }");
   EXPECT_EQ(alt_perf.getMode(), "alt");
 
   std::map<std::string, std::string> map =
-      std::map<std::string, std::string>{{"test0", "cat0"}, {"test1", "cat1"}};
+      std::map<std::string, std::string>{{"a0", "cat0"}, {"a1", "cat1"}};
   AlternativesPerformance alt_perf2 = AlternativesPerformance(perf_table, map);
   std::ostringstream os2;
   os2 << alt_perf2;
   EXPECT_EQ(os2.str(),
             "AlternativesPerformance( PerformanceTable[ Performance: Perf( "
-            "name : test0, crit : a0, value "
-            ": 0 ) Perf( name : test0, crit : a1, value : 0 ) | "
-            "Performance: Perf( name : test1, crit : a0, value : 0 ) Perf( "
-            "name : test1, crit : a1, value : 0 ) | ], AlternativesAssignment{ "
-            "test0->cat0"
-            " test1->cat1 }");
+            "name : a0, crit : crit0, value "
+            ": 0 ) Perf( name : a0, crit : crit1, value : 0 ) | "
+            "Performance: Perf( name : a1, crit : crit0, value : 0 ) Perf( "
+            "name : a1, crit : crit1, value : 0 ) | ], AlternativesAssignment{ "
+            "a0->cat0"
+            " a1->cat1 }");
   EXPECT_EQ(alt_perf2.getMode(), "alt");
 }
 
 TEST(TestAlternativesPerformance, TestConstructorWithPerfTableMapErrors) {
   std::vector<Performance> perf_vect;
-  Criteria crit = Criteria(2, "a");
-  perf_vect.push_back(Performance(crit, "test0"));
-  perf_vect.push_back(Performance(crit, "test1"));
+  Criteria crit = Criteria(2, "crit");
+  perf_vect.push_back(Performance(crit, "a0"));
+  perf_vect.push_back(Performance(crit, "a1"));
   PerformanceTable perf_table = PerformanceTable(perf_vect);
   std::map<std::string, std::string> map =
-      std::map<std::string, std::string>{{"test12", "cat0"}, {"test1", "cat1"}};
+      std::map<std::string, std::string>{{"a12", "cat0"}, {"a1", "cat1"}};
 
   try {
     AlternativesPerformance alt_perf = AlternativesPerformance(perf_table, map);
@@ -165,9 +165,9 @@ TEST(TestAlternativesPerformance, TestConstructorWithPerfTableMapErrors) {
 
 TEST(TestAlternativesPerformance, TestCopyConstructor) {
   std::vector<Performance> perf_vect;
-  Criteria crit = Criteria(2, "a");
-  perf_vect.push_back(Performance(crit, "test0"));
-  perf_vect.push_back(Performance(crit, "test1"));
+  Criteria crit = Criteria(2, "crit");
+  perf_vect.push_back(Performance(crit, "a0"));
+  perf_vect.push_back(Performance(crit, "a1"));
   PerformanceTable perf_table = PerformanceTable(perf_vect);
   AlternativesPerformance alt_perf = AlternativesPerformance(perf_table);
 
@@ -181,11 +181,10 @@ TEST(TestAlternativesPerformance, TestCopyConstructor) {
 }
 
 TEST(TestAlternativesPerformance, TestGetAssignmentMap) {
-  Criteria crit = Criteria(2, "a");
+  Criteria crit = Criteria(2, "crit");
   std::map<std::string, std::string> map =
-      std::map<std::string, std::string>{{"test0", "cat0"}, {"test1", "cat1"}};
-  AlternativesPerformance alt_perf =
-      AlternativesPerformance(2, crit, "test", map);
+      std::map<std::string, std::string>{{"a0", "cat0"}, {"a1", "cat1"}};
+  AlternativesPerformance alt_perf = AlternativesPerformance(2, crit, "a", map);
 
   std::map<std::string, std::string> map2 =
       alt_perf.getAlternativesAssignments();
@@ -194,10 +193,10 @@ TEST(TestAlternativesPerformance, TestGetAssignmentMap) {
 }
 
 TEST(TestAlternativesPerformance, TestSetAssignmentMap) {
-  Criteria crit = Criteria(2, "a");
-  AlternativesPerformance alt_perf = AlternativesPerformance(2, crit, "test");
+  Criteria crit = Criteria(2, "crit");
+  AlternativesPerformance alt_perf = AlternativesPerformance(2, crit, "a");
   std::map<std::string, std::string> map =
-      std::map<std::string, std::string>{{"test0", "cat0"}, {"test1", "cat1"}};
+      std::map<std::string, std::string>{{"a0", "cat0"}, {"a1", "cat1"}};
 
   alt_perf.setAlternativesAssignments(map);
 
@@ -205,10 +204,10 @@ TEST(TestAlternativesPerformance, TestSetAssignmentMap) {
 }
 
 TEST(TestAlternativesPerformance, TestSetAssignmentMapErrors) {
-  Criteria crit = Criteria(2, "a");
-  AlternativesPerformance alt_perf = AlternativesPerformance(2, crit, "test");
+  Criteria crit = Criteria(2, "crit");
+  AlternativesPerformance alt_perf = AlternativesPerformance(2, crit, "a");
   std::map<std::string, std::string> map = std::map<std::string, std::string>{
-      {"testerror0", "cat0"}, {"test1", "cat1"}};
+      {"testerror0", "cat0"}, {"a1", "cat1"}};
 
   try {
     alt_perf.setAlternativesAssignments(map);
@@ -222,35 +221,32 @@ TEST(TestAlternativesPerformance, TestSetAssignmentMapErrors) {
 }
 
 TEST(TestAlternativesPerformance, TestGetAlternativeAssignment) {
-  Criteria crit = Criteria(2, "a");
+  Criteria crit = Criteria(2, "crit");
   std::map<std::string, std::string> map =
-      std::map<std::string, std::string>{{"test0", "cat0"}, {"test1", "cat1"}};
-  AlternativesPerformance alt_perf =
-      AlternativesPerformance(2, crit, "test", map);
+      std::map<std::string, std::string>{{"a0", "cat0"}, {"a1", "cat1"}};
+  AlternativesPerformance alt_perf = AlternativesPerformance(2, crit, "a", map);
 
-  std::string alt_test0_assignment = alt_perf.getAlternativeAssignment("test0");
+  std::string alt_a0_assignment = alt_perf.getAlternativeAssignment("a0");
 
-  EXPECT_EQ(alt_test0_assignment, "cat0");
+  EXPECT_EQ(alt_a0_assignment, "cat0");
 }
 
 TEST(TestAlternativesPerformance, TestSetAlternativeAssignment) {
-  Criteria crit = Criteria(2, "a");
+  Criteria crit = Criteria(2, "crit");
   std::map<std::string, std::string> map =
-      std::map<std::string, std::string>{{"test0", "cat0"}, {"test1", "cat1"}};
-  AlternativesPerformance alt_perf =
-      AlternativesPerformance(2, crit, "test", map);
+      std::map<std::string, std::string>{{"a0", "cat0"}, {"a1", "cat1"}};
+  AlternativesPerformance alt_perf = AlternativesPerformance(2, crit, "a", map);
 
-  alt_perf.setAlternativeAssignment("test0", "cat5");
+  alt_perf.setAlternativeAssignment("a0", "cat5");
 
-  EXPECT_EQ(alt_perf.getAlternativeAssignment("test0"), "cat5");
+  EXPECT_EQ(alt_perf.getAlternativeAssignment("a0"), "cat5");
 }
 
 TEST(TestAlternativesPerformance, TestSetAlternativeAssignmentError) {
-  Criteria crit = Criteria(2, "a");
+  Criteria crit = Criteria(2, "crit");
   std::map<std::string, std::string> map =
-      std::map<std::string, std::string>{{"test0", "cat0"}, {"test1", "cat1"}};
-  AlternativesPerformance alt_perf =
-      AlternativesPerformance(2, crit, "test", map);
+      std::map<std::string, std::string>{{"a0", "cat0"}, {"a1", "cat1"}};
+  AlternativesPerformance alt_perf = AlternativesPerformance(2, crit, "a", map);
 
   try {
     alt_perf.setAlternativeAssignment("testerror0", "cat5");
